8677b73c5872ba9cddaec0173ed2f4f313866b2c
Elrime <92987050+Elrime@users.noreply.github.com>
2026-05-22 17:14:06 +0300

    Add solution for expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    x = np.array(x)
+    p = np.array(p)
+    E = np.sum(x * p)
+    if np.sum(p) < 1:
+        raise ValueError
+    else:
+        return E
+   
